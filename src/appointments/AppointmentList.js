8b9357f414c1c69ad01cf06768712992e85500be
tellfinn <wib0915@gmx.de>
2019-10-02 16:34:11 +0200

    Move sortfunction to AppointmentPage

--- src/appointments/AppointmentList.js
+++ src/appointments/AppointmentList.js
@@ -1,43 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import styled from 'styled-components/macro'
-import Appointment from './Appointment'
-import { getAppointments } from './services'
 
-export default function AppointmentList() {
-  const [appointments, setAppointments] = useState([])
-
-  useEffect(() => {
-    getAppointments().then(setAppointments)
-  }, [])
-
-  return <AppointmentListStyled>{sortByDate()}</AppointmentListStyled>
-
-  function sortByDate() {
-    const sortedAppointments = appointments.slice().sort((a, b) => {
-      return new Date(a.date) - new Date(b.date)
-    })
-    return sortedAppointments.map(appointment => (
-      <Appointment key={appointment._id} {...appointment} />
-    ))
-  }
-
-  function sortByTime() {
-    const sortedAppointments = appointments.slice().sort((a, b) => {
-      return new Date(a.date).getHours() - new Date(b.date).getHours()
-    })
-    return sortedAppointments.map(appointment => (
-      <Appointment key={appointment._id} {...appointment} />
-    ))
-  }
-
-  function sortByClinic() {
-    const sortedAppointments = appointments.slice().sort((a, b) => {
-      return a.clinic > b.clinic
-    })
-    return sortedAppointments.map(appointment => (
-      <Appointment key={appointment._id} {...appointment} />
-    ))
-  }
+export default function AppointmentList({ children }) {
+  return <AppointmentListStyled>{children}</AppointmentListStyled>
 }
 
 const AppointmentListStyled = styled.ul`
--- src/appointments/AppointmentPage.js
+++ src/appointments/AppointmentPage.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { getAppointments } from './services'
 import Page from '../common/Page'
 import AppointmentList from './AppointmentList'
+import Appointment from './Appointment'
 import SortByBar from './SortByBar'
 
 export default function AppointmentPage() {
+  let [appointments, setAppointments] = useState([])
+
+  useEffect(() => {
+    getAppointments().then(setAppointments)
+  }, [])
+
   return (
     <Page>
       <SortByBar />
-      <AppointmentList></AppointmentList>
+      <AppointmentList>{renderAppointments('date')}</AppointmentList>
     </Page>
   )
+
+  function renderAppointments(sortByProp) {
+    let sortedAppointments
+    if (sortByProp === 'date') {
+      sortedAppointments = appointments.slice().sort((a, b) => {
+        return new Date(a.date) - new Date(b.date)
+      })
+    } else if (sortByProp === 'time') {
+      sortedAppointments = appointments.slice().sort((a, b) => {
+        return new Date(a.date).getHours() - new Date(b.date).getHours()
+      })
+    } else if (sortByProp === 'clinic') {
+      sortedAppointments = appointments.slice().sort((a, b) => {
+        return a.clinic > b.clinic
+      })
+    }
+
+    return sortedAppointments.map(appointment => (
+      <Appointment key={appointment._id} {...appointment} />
+    ))
+  }
 }
